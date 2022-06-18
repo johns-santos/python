a1912938c4a01278944796ed5cca934975963e5f
johns-santos <johnsanthony.santos@gmail.com>
2022-06-18 00:04:51 -0500

    added ascii art to hangman game

--- basic_concepts/day_07/hangman_game.py
+++ basic_concepts/day_07/hangman_game.py
@@ -61,16 +61,27 @@ stages = ['''
 =========
 ''']
 
+
+
+
 end_of_game = False
 # Generate a random word
-word_list = ["Sunray", "vacation", "Christmas", "Marine", "apple", "rocky"]
+hint = ["What can’t talk but will reply when spoken to?",
+"The more of this there is, the less you see. What is it?",
+"I have branches, but no fruit, trunk or leaves. What am I?",
+"Poor people have it. Rich people need it. If you eat it you die. What is it?",
+"Spelled forwards I’m what you do every day, spelled backward I’m something you hate. What am I?"]
+word_list = ["echo", "darkness", "bank","nothing","live"]
 # Generate blanks required for each letter in word
 chosen_word = random.choice(word_list).lower()
 lives = 6
 
+hint_index = word_list.index(chosen_word)
 
+print("\n===========================================")
+print(f"HINT: {hint[hint_index]}")
+print("===========================================\n")
 
-print(f"Solution: {chosen_word}")
 
 display = []
 word_length = len(chosen_word)
@@ -81,7 +92,7 @@ print(display)
 
 # Allow user to guess until blanks are eliminated
 while not end_of_game:
-    user_guess = input("Guess a letter: ").lower()
+    user_guess = input("\nGuess a letter: \n ").lower()
         
     #CHECK GUESS - if CORRECT replace blank with letter
     for position in range(word_length):
@@ -95,20 +106,27 @@ while not end_of_game:
 # If "_" have been eliminated end game
     if "_" not in display:
         end_of_game = True
-        print("You Win!!!!")
+        print("\n===========================================")
+        print("You WIN!!!!")
+        print("===========================================\n")
 
     if user_guess not in chosen_word:
-            lives = lives - 1
-            print(stages[lives+1])
+            lives -= 1
+            if lives == 0:
+                print(stages[lives])
+                print("\n===========================================")
+                print(f"{lives} lives remaing")
+                print("You LOSE!!!!")
+                print("===========================================\n")
+                break
 
-    if lives == 0:
-        print(stages[lives])
-        print("You Lose!")
-        break
-
-
-    
-    print(lives)
+    if lives > 0:
+      print(f"\nHINT: {hint[hint_index]}\n")
+      print("\n===========================================")
+      print(f"{lives} lives remaing")
+      print("===========================================\n")
+    else:
+      break
 
    
             
