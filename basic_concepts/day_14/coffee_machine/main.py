f1d1758f828a3f20fcdab8b122b1b5be42071280
John Santos <57374506+johns-santos@users.noreply.github.com>
2022-12-22 03:22:57 -0600

    Coffee Machine OOP Project

---
+++ basic_concepts/day_14/coffee_machine/main.py
@@ -0,0 +1,51 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+coffee_machine = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+def coffeeReport():
+    return coffee_machine.report()
+
+def moneyReport():
+    return money_machine.report()
+
+def getMenu():
+    return menu.get_items()
+
+def findDrink(d):
+    return menu.find_drink(d)
+
+
+# Prompt user for input while "completed" equals 0
+completed = 0
+while completed == 0:
+    options = getMenu()   
+    userOrder = input(f"\nWhat would you like? ({options}) : ").lower()
+    
+    # Validate user input.
+    if userOrder != "report" and "off" and "latte" and "espresso" and "cappachino":
+        print(f"\nInput not valid!\n")
+    # Print reports if user types "report"
+    elif userOrder == "report":
+        print("")
+        coffeeReport()
+        print("")
+        moneyReport()
+        print("")
+    # Stop program when user types "off" by seting completed to 1
+    elif userOrder == "off":
+        completed += 1
+    else:
+        drink = findDrink(userOrder)
+        # Check if there are sufficient resources to make drink
+        if coffee_machine.is_resource_sufficient(drink):
+             print("")
+        # Count coins provided by customer and make drink if payment is sufficient.
+        if money_machine.make_payment(drink.cost):
+            print("")
+            coffee_machine.make_coffee(drink)
+
+    
