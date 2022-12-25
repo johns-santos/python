b7ead06419bae8836e1990e96ae03efe50058c46
John Santos <57374506+johns-santos@users.noreply.github.com>
2022-12-25 01:53:50 -0600

    Added function to class

--- basic_concepts/day_14/OOP/HP_Students/main.py
+++ basic_concepts/day_14/OOP/HP_Students/main.py
@@ -3,25 +3,45 @@
 class Student:
      # Clas can have a function called a METHOD.
      # "__init__" function INSTANCIATES the OBJECT (Stores in memory)
-     def __init__(self, name, house):
+    def __init__(self, name, school, mascott):
+         # Check attributes and catch errors with "raise"
+         if not name:
+             raise ValueError("Missing Name.")
+         if school not in ["EHS", "TJHS", "FTHS"]:
+             raise ValueError("Invalid school")
          # Declare INSTANCE variables
          self.name = name
-         self.house = house
-
+         self.school = school
+         self.mascott = mascott
+    
+    # Object method "__str__" returns class or attributes as a strings   
+    def __str__(self):
+        return f"{self.name} from {self.school}"
+    
+    def spirit(self):
+        match self.mascott:
+            case "bear":
+                return "👹"
+            case "mustang":
+                return "🙊"
+            case "buffallo":
+                return "😾"
+            # CASE for empty input
+            case _:
+                return ":("
+        
+    
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}.")
+    print("School Spirit!!!!!!")
+    print(student.spirit())
 
 def get_student():
-    # student = Student()
-    # student.name = input("Name: ")
-    # student.house = input("House: ")
     name = input("Name: ")
-    house = input("House: ")
+    school = input("School: ")
+    mascott = input("Mascot: ")
     # Construct the object - attributes are added by ("__init__")
-    student = Student(name, house)
-    return student
+    return Student(name, school, mascott)
     
-
 if __name__ == "__main__":
     main()
