5701fb7e2ecbc6e4209e5632bd1c81b95485dffc
John Santos <57374506+johns-santos@users.noreply.github.com>
2022-12-28 18:39:02 -0600

    completed student

--- basic_concepts/day_14/OOP/HP_Students/main.py
+++ basic_concepts/day_14/OOP/HP_Students/main.py
@@ -1,9 +1,8 @@
-
 # Class is a OBJECT - It is a data type
 class Student:
      # Clas can have a function called a METHOD.
      # "__init__" function INSTANCIATES the OBJECT (Stores in memory)
-    def __init__(self, name, school, mascott):
+    def __init__(self, name, school):
          # Check attributes and catch errors with "raise"
          if not name:
              raise ValueError("Missing Name.")
@@ -11,37 +10,20 @@ class Student:
              raise ValueError("Invalid school")
          # Declare INSTANCE variables
          self.name = name
-         self.school = school
-         self.mascott = mascott
-    
+         self.school = school     
     # Object method "__str__" returns class or attributes as a strings   
     def __str__(self):
         return f"{self.name} from {self.school}"
     
-    def spirit(self):
-        match self.mascott:
-            case "bear":
-                return "👹"
-            case "mustang":
-                return "🙊"
-            case "buffallo":
-                return "😾"
-            # CASE for empty input
-            case _:
-                return ":("
-        
-    
 def main():
     student = get_student()
-    print("School Spirit!!!!!!")
-    print(student.spirit())
-
+    print(student)
+  
 def get_student():
     name = input("Name: ")
     school = input("School: ")
-    mascott = input("Mascot: ")
     # Construct the object - attributes are added by ("__init__")
-    return Student(name, school, mascott)
+    return Student(name, school)
     
 if __name__ == "__main__":
     main()
