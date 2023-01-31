94a2700ebdfcecf183909f4d971c2bec5a98580b
John Santos <57374506+johns-santos@users.noreply.github.com>
2023-01-30 19:02:20 -0600

    coffee maker main updated. Working copy

--- basic_concepts/day_14/coffee_machine/main.py
+++ basic_concepts/day_14/coffee_machine/main.py
@@ -1,47 +1,29 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 coffee_machine = CoffeeMaker()
 money_machine = MoneyMachine()
 menu = Menu()
-
-def coffeeReport():
-    return coffee_machine.report()
-
-def moneyReport():
-    return money_machine.report()
-
-def getMenu():
-    return menu.get_items()
-
-def findDrink(d):
-    return menu.find_drink(d)
-
 # Prompt user for input while "completed" equals 0
 completed = 0
 while completed == 0:
-    options = getMenu()   
-    userOrder = input(f"\nWhat would you like? ({options}) : ").lower()
+    options = menu.get_items()
     
-    # Validate user input.
-    if userOrder != "report" and userOrder != "off" and userOrder != "latte" and userOrder != "espresso" and userOrder != "cappuccino":
-        print(f"\nInput not valid!\n")
-    # Print reports if user types "report"
-    elif userOrder == "report":
-        coffeeReport()
-        moneyReport()
-    # Stop program when user types "off" by seting completed to 1
-    elif userOrder == "off":
-        completed += 1
-    else:
-        drink = findDrink(userOrder)
-        # Check if there are sufficient resources to make drink
-        if coffee_machine.is_resource_sufficient(drink):
-             print("")
-        # Count coins provided by customer and make drink if payment is sufficient.
-        if money_machine.make_payment(drink.cost):
-            print("")
-            coffee_machine.make_coffee(drink)
-
-    
+    while True:
+        
+        try:
+            choice = input(f"\nWhat would you like? ({options}) : ").lower()
+            if choice == "off":
+                completed = 1
+            elif choice == "report":
+                coffee_machine.report()
+                money_machine.report()
+            else:
+                drink = menu.find_drink(choice)
+            if coffee_machine.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_machine.make_coffee(drink)
+        except AttributeError:
+            pass
+        except ValueError: 
+            pass
