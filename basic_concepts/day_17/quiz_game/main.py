6d2d2abed1eaf798ac69f0780b3254f6da7732e1
John Santos <57374506+johns-santos@users.noreply.github.com>
2023-02-04 00:54:51 -0600

    updated question_data and question_bank

--- basic_concepts/day_17/quiz_game/main.py
+++ basic_concepts/day_17/quiz_game/main.py
@@ -5,8 +5,8 @@ from quiz_brain import QuizBrain
 question_bank = []
 # For loop - for item in list
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
        
@@ -14,3 +14,7 @@ quiz = QuizBrain(question_bank)
 
 while quiz.questions_remaining():
     quiz.next_question()
+    
+
+print("You've completed the quiz.")
+print(f"Your final score was: {quiz.question_score}/{quiz.question_number}\n")
