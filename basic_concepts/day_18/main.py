a62eeafd4106c19f6ce42a494a4db96d3e3f62cc
John Santos <57374506+johns-santos@users.noreply.github.com>
2023-02-04 01:20:31 -0600

    Timmy The Turtle project

---
+++ basic_concepts/day_18/main.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+from turtle import Screen
+
+# Required in order to open window
+screen = Screen()
+screen.colormode(255)
+
+# Instatinate turtle
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color(139, 0, 139)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
