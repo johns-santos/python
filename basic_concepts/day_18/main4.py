353988598209e29f88f43ee34296f2fdb2aaf11c
John Santos <57374506+johns-santos@users.noreply.github.com>
2023-02-07 23:11:25 -0600

    main4.py contains a random_color function

--- basic_concepts/day_18/main4.py
+++ basic_concepts/day_18/main4.py
@@ -2,22 +2,27 @@ from turtle import Turtle, Screen
 import random
 
 screen = Screen()
+# Color mode allows RGB range between 0 and 255.
+screen.colormode(255)
 
 t = Turtle()
 t.pensize(10)
+direction = [0,90,180, 270]
+# colors = ['red', 'pink', 'black', 'blue', 'magenta', 'cyan','purple','orange', 'green']
 
-colors = ['red', 'pink', 'black', 'blue', 'magenta', 'cyan','purple','orange', 'green']
-direction = [0, 90, 180, 270]
-
+def random_color():
+    return (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+    
 
 def random_walk(num):
     for i in range(1, num):
-        t.color(random.choice(colors))
-        t.forward(25)
+        # t.color(random.choice(colors))
+        t.pencolor(random_color())
+        t.forward(15)
         t.setheading(random.choice(direction))
-        # t.speed(i++1)
+        t.speed(i++1)
         
-random_walk(250)
+random_walk(500)
 
 screen.exitonclick()
 
