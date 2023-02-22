d39381c4c51b2f5e116bed74e2d657150bb62a7f
John Santos <57374506+johns-santos@users.noreply.github.com>
2023-02-22 02:07:46 -0600

    completed scoreboard and score update

--- basic_concepts/day_20/snake_game/main.py
+++ basic_concepts/day_20/snake_game/main.py
@@ -1,36 +1,52 @@
-from snake import Snake
 from turtle import Turtle, Screen
+from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Collide")
+screen.title("Snake Colide")
+
 # Turn off tracer
 screen.tracer(0)
 
 snake = Snake()
+# 3) Create food
+food = Food()
+# 5) Create scoreboard
+scoreboard = ScoreBoard()
 
-# Make snake move - forward
+    
+#2) Control snake across screen  
+screen.listen() 
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down") 
+screen.onkey(snake.left, "Left") 
+screen.onkey(snake.right, "Right") 
+
+#1) Make snake move - forward
 game_on = True
 while game_on:
+    # Refresh
     screen.update()
+    # Refresh control rate
     time.sleep(0.1)
-    
     snake.move()
-
-
-####===== TASK LEFT TO-DO 
-# Control snake across screen
-
-# Create food & detect collision with food
-
-# Create scoreboard
-
-# Detect collision with wall - Game over
-
-# Detect collision with tail - Game over
-
-
+    
+    # 4) detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        # 5) Refresh scoreboard
+        scoreboard.increase_score()
+       
+    #6) Detect collision with wall - Game over
+    if snake.head.xcor() == 310 or snake.head.xcor() == -310:
+        game_on = False
+        
+    if snake.head.ycor() == 310 or snake.head.ycor() == - 310:
+        game_on = False
+       
 
 screen.exitonclick()
