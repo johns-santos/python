0ec51faca86539833ce050ab5644be265874ec4d
John Santos <57374506+johns-santos@users.noreply.github.com>
2023-03-08 22:28:51 -0600

    updating CarManager

--- basic_concepts/day_23/main.py
+++ basic_concepts/day_23/main.py
@@ -2,22 +2,32 @@ import time
 from turtle import Screen, Turtle
 from player import Player
 from car_manager import CarManager
-from scoreboard import Scoreboard
+# from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
 ## 1) Create a turtle that starts at the bottom of the screen 
-player_1 = Player()
+player = Player()
+car_manager = CarManager()
 
 ## 2) Listen for the "UP" keypress
 screen.listen()
-screen.onkey(player_1.move_forward, "Up")
-screen.onkey(player_1.move_back, "Down")
+screen.onkey(player.move_forward, "Up")
+screen.onkey(player.move_back, "Down")
+
 
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    
+    
+    
+    
