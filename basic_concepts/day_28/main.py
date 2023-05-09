e64f541be5e2032768992ef9ec0fb67dd1eabf56
John Santos <57374506+johns-santos@users.noreply.github.com>
2023-05-09 00:14:37 -0500

    completed UI

--- basic_concepts/day_28/main.py
+++ basic_concepts/day_28/main.py
@@ -20,11 +20,25 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50), bg=YELLOW)
+title_label.grid(column=1, row=0)
+
+
 canvas = Canvas(width=200, height=224, background=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="./tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(101, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+
+start_button = Button(text="Start", highlightbackground=YELLOW)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightbackground=YELLOW)
+reset_button.grid(column=2, row=2)
+
+
+
 
 window.mainloop()
 
